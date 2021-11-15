dce6bc9c6b266e8001f70f05c29e377abf2a7593
1995-june <2193552264@qq.com>
2021-11-15 18:50:31 +0800

    添加控制台提示‘

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import scrollMixin from '@/mixins/scroll.js' // 全局注册 mixins
 
 import '@/static/fonts/iconfont.css'
 import '@/utils/rem.js' // rem适配
+import '@/utils/console.js'
 import api from '@/api' // ajax
 import 'reset-css' // 默认样式
 import animate from 'animate.css' // 动画
@@ -18,6 +19,7 @@ import aoutUseComponents from './plugins/aoutUseComponents' // 全局注册
 Vue.use(aoutUseComponents) // 全局注册插件
 Vue.use(VueAwesomeSwiper) // 轮播图插件
 Vue.use(scrollMixin) // 米心插件
+// Vue.use(console) // 米心插件
 
 Vue.prototype.$api = api
 Vue.prototype.$animate = animate
